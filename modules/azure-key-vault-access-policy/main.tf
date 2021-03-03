0ec0e6615487d60cc95840a267edc4c18cd29bc3
Luke Fritz <lukefritz@gmail.com>
2021-03-03 13:41:39 -0600

    EN-465: Remove data source from module intended to be dynamic (breaking)

--- modules/azure-key-vault-access-policy/main.tf
+++ modules/azure-key-vault-access-policy/main.tf
@@ -6,11 +6,9 @@ terraform {
   required_version = ">= 0.12"
 }
 
-data "azurerm_client_config" "current" {}
-
 resource "azurerm_key_vault_access_policy" "policy" {
   key_vault_id   = var.key_vault_id
-  tenant_id      = data.azurerm_client_config.current.tenant_id
+  tenant_id      = var.tenant_id
   object_id      = var.object_id
   application_id = var.application_id
 
